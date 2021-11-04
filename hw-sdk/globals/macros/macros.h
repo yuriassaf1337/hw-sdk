72bd776f54c8804ca06a7b8dc79c6a608e7e9f6f
yuriassaf1337 <38926035+yuriassaf1337@users.noreply.github.com>
2021-11-03 22:49:58 -0300

    adress struct for sigs, vfunc, hash -> HASH

--- hw-sdk/globals/macros/macros.h
+++ hw-sdk/globals/macros/macros.h
@@ -3,6 +3,25 @@
 #ifndef MACROS_HPP
 #	define MACROS_HPP
 
-#	define hash( s ) fnv::hash( s )
+#	define HASH( s ) fnv::hash( s )
+
+#	define CLIENT_DLL              HASH( "client.dll" )
+#	define ENGINE_DLL              HASH( "engine.dll" )
+#	define LOCALIZE_DLL            HASH( "localize.dll" )
+#	define MATERIALSYSTEM_DLL      HASH( "materialsystem.dll" )
+#	define VGUI_DLL                HASH( "vguimatsurface.dll" )
+#	define VGUI2_DLL               HASH( "vgui2.dll" )
+#	define SHADERAPIDX9_DLL        HASH( "shaderapidx9.dll" )
+#	define GAMEOVERLAYRENDERER_DLL HASH( "gameoverlayrenderer.dll" )
+#	define PHYSICS_DLL             HASH( "vphysics.dll" )
+#	define VSTDLIB_DLL             HASH( "vstdlib.dll" )
+#	define TIER0_DLL               HASH( "tier0.dll" )
+#	define INPUTSYSTEM_DLL         HASH( "inputsystem.dll" )
+#	define STUDIORENDER_DLL        HASH( "studiorender.dll" )
+#	define DATACACHE_DLL           HASH( "datacache.dll" )
+#	define STEAM_API_DLL           HASH( "steam_api.dll" )
+#	define MATCHMAKING_DLL         HASH( "matchmaking.dll" )
+#	define SERVER_DLL              HASH( "server.dll" )
+#	define SERVERBROWSER_DLL       HASH( "serverbrowser.dll" )
 
 #endif
--- hw-sdk/utils/utils.h
+++ hw-sdk/utils/utils.h
@@ -1,5 +1,6 @@
 #include "../dependencies/lazy_importer/lazy_importer.h"
 #include "helpers/primitive_definitions/primitives.h"
+#include "modules/address/address.h"
 
 #include <Windows.h>
 #include <cstdint>
@@ -12,5 +13,21 @@ namespace utils
 {
 	void sleep( std::uint32_t time );
 	BOOL cheat_create_thread( DWORD WINAPI function( void* ), void* param );
+
+	// templated functions
+
+	template< typename T = address >
+	inline T get_v_func( address thisptr, std::uint32_t idx )
+	{
+		return ( T )( thisptr.to< T* >( )[ idx ] );
+	}
+
+	template< typename T, std::uint32_t I, typename... VA >
+	inline T call_v_func( address thisptr, VA... args )
+	{
+		using FN = T( __thiscall* )( unk, decltype( args )... );
+		return thisptr.to< FN* >( )[ I ]( thisptr, args... );
+	}
+
 } // namespace utils
 #endif // ! UTILS_HPP
