049dd156a6493f4488e60d655358251d08601876
Liga <liga@wednesday.wtf>
2021-11-12 11:49:00 -0800

    create_move return type wrong.

--- hw-sdk/globals/hooks/create_move/create_move.cpp
+++ hw-sdk/globals/hooks/create_move/create_move.cpp
@@ -2,9 +2,7 @@
 
 #include "create_move.h"
 
-bool __fastcall hooks::create_move::create_move_detour( sdk::i_client_dll* _this, void* edx, int sequence_number, float input_sample_frametime, bool active )
+void __fastcall hooks::create_move::create_move_detour( sdk::i_client_dll* _this, void* edx, int sequence_number, float input_sample_frametime, bool active )
 {
 	hooks::create_move_hook.call_original< bool >( _this, edx, input_sample_frametime, active );
-
-	return false;
 }
