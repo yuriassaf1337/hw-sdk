8853e90c361055d1fc8d6b51a1fff31c9161c44a
Liga <liga@wednesday.wtf>
2021-11-16 08:50:38 -0800

    Fixed uninjection LAWL!

--- hw-sdk/globals/hotwheels/hotwheels.cpp
+++ hw-sdk/globals/hotwheels/hotwheels.cpp
@@ -18,8 +18,10 @@ DWORD WINAPI hotwheels::init( void* module_handle )
 
 	ENFORCE_FAILURE( hooks::init( ), "Failed to init hooks" );
 
-	if ( g_input.is_key_pressed( VK_INSERT ) )
+	while ( g_input.is_key_pressed( VK_INSERT ) ) {
+		utils::sleep( 10 );
 		LI_FN( CreateThread )( nullptr, 0, reinterpret_cast< LPTHREAD_START_ROUTINE >( unload ), nullptr, 0, nullptr );
+	}
 
 	return 0;
 }
