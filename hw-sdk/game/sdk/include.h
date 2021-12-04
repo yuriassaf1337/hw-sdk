2079c6d3192028998fab853ceee19a51a7ac7643
yuriassaf1337 <38926035+yuriassaf1337@users.noreply.github.com>
2021-12-04 18:47:50 -0300

    @windows-fryer im having include issues with engine trace, not sure what it is but i alr added the full class and needed stuff for it, can u fix

--- hw-sdk/game/sdk/include.h
+++ hw-sdk/game/sdk/include.h
@@ -5,6 +5,7 @@
 #include "classes/i_client_dll.h"
 #include "classes/i_client_entity_list.h"
 #include "classes/i_cvar.h"
+#include "classes/i_engine_trace.h"
 #include "classes/i_global_vars.h"
 #include "classes/i_prediction.h"
-#include "classes/iv_engine_client.h"
+#include "enums/trace_type.h"
