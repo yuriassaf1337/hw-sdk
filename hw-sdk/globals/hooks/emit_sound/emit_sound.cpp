a786cc9baf41160fc7963ad498b42da86b86832d
Liga <ligathetranny@protonmail.com>
2021-12-04 18:26:20 -0800

    Fixed emit sound stufff

--- hw-sdk/globals/hooks/emit_sound/emit_sound.cpp
+++ hw-sdk/globals/hooks/emit_sound/emit_sound.cpp
@@ -4,23 +4,21 @@
 
 void __fastcall hooks::emit_sound::emit_sound_detour( void* ecx, void* edx, void* filter, int entity_index, int channel, const char* sound_entry,
                                                       unsigned int sound_entry_hash, const char* sample, float volume, int seed, int sound_level,
-                                                      int flags, int pitch, const math::vec3& origin, const math::vec3& direction,
+                                                      int flags, int pitch, const math::vec3* origin, const math::vec3* direction,
                                                       void* utl_vec_origins, bool update_positions, float soundtime, int speaker_entity,
                                                       void* sound_params )
-
 {
-	static auto call_original = [ & ]( ) {
-		hooks::emit_sound_hook.call_original< void >( ecx, edx, filter, entity_index, channel, sound_entry, sound_entry_hash, sample, volume, seed,
-		                                              sound_level, flags, pitch, std::cref( origin ), std::cref( direction ), utl_vec_origins,
-		                                              update_positions, soundtime, speaker_entity, sound_params );
-	};
-
 	if ( !g_ctx.local || !g_ctx.local->is_alive( ) && g_interfaces.engine->connected_safe( ) )
-		return call_original( );
+		 goto CALL_ORIGINAL;
 
 	// dont play sounds twice while running ProcessMove
-	if ( g_interfaces.prediction->in_prediction( ) && g_interfaces.prediction->is_first_time_predicted( ) )
-		return;
+	//if ( g_interfaces.prediction->in_prediction( ) && g_interfaces.prediction->is_first_time_predicted( ) )
+	//	return;
 
-	call_original( );
+	goto CALL_ORIGINAL;
+
+CALL_ORIGINAL:
+		hooks::emit_sound_hook.call_original< void >( ecx, edx, filter, entity_index, channel, sound_entry, sound_entry_hash, sample, volume, seed,
+		                                            sound_level, flags, pitch, origin, direction, utl_vec_origins,
+		                                            update_positions, soundtime, speaker_entity, sound_params );
 }
--- hw-sdk/globals/hooks/emit_sound/emit_sound.h
+++ hw-sdk/globals/hooks/emit_sound/emit_sound.h
@@ -13,12 +13,12 @@ namespace hooks
 	CREATE_HOOK_HELPER( emit_sound_hook,
 	                    void( __fastcall )( void* ecx, void* edx, void* filter, int entity_index, int channel, const char* sound_entry,
 	                                        unsigned int sound_entry_hash, const char* sample, float volume, int seed, int sound_level, int flags,
-	                                        int pitch, const math::vec3& origin, const math::vec3& direction, void* utl_vec_origins,
+	                                        int pitch, const math::vec3* origin, const math::vec3* direction, void* utl_vec_origins,
 	                                        bool update_positions, float soundtime, int speaker_entity, void* sound_params ) );
 	struct emit_sound {
 		static void __fastcall emit_sound_detour( void* ecx, void* edx, void* filter, int entity_index, int channel, const char* sound_entry,
 		                                          unsigned int sound_entry_hash, const char* sample, float volume, int seed, int sound_level,
-		                                          int flags, int pitch, const math::vec3& origin, const math::vec3& direction, void* utl_vec_origins,
+		                                          int flags, int pitch, const math::vec3* origin, const math::vec3* direction, void* utl_vec_origins,
 		                                          bool update_positions, float soundtime, int speaker_entity, void* sound_params );
 
 		static void init( )
