812530ea4e75343ff8dacb4a89a71b3d5c638064
Liga <ligathetranny@protonmail.com>
2021-12-23 14:55:53 -0800

    Added animated wireframe stuff

--- hw-sdk/game/sdk/classes/i_material_system.h
+++ hw-sdk/game/sdk/classes/i_material_system.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../../../utils/vfunc/vfunc.h"
+#include "../structs/i_material.h"
 #include "../structs/i_texture.h"
 #include "i_mat_render_ctx.h"
 
@@ -26,6 +27,13 @@ namespace sdk
 		sdk::i_texture* find_texture( const char* name, const char* group_name, bool complain = true, int unk = 1 )
 		{
 			return virtual_func::call< sdk::i_texture*, const char*, const char*, bool, int >( this, 91, name, group_name, complain, unk );
+		}
+
+		sdk::i_material* find_material( const char* name, const char* group_name = nullptr, bool complain = true,
+		                                const char* complain_prefix = nullptr )
+		{
+			return virtual_func::call< sdk::i_material*, const char*, const char*, bool, const char* >( this, 84, name, group_name, complain,
+			                                                                                            complain_prefix );
 		}
 	};
 } // namespace sdk
--- hw-sdk/globals/hooks/draw_model_execute/draw_model_execute.cpp
+++ hw-sdk/globals/hooks/draw_model_execute/draw_model_execute.cpp
@@ -5,5 +5,33 @@
 void __fastcall hooks::draw_model_execute::draw_model_execute_detour( void* ecx, void* edx, void* context, void* state, sdk::model_render_info& info,
                                                                       math::matrix_3x4* custom_bone_to_world )
 {
+	static sdk::i_material* animated_wireframe;
+
+	if ( !animated_wireframe ) {
+		std::ofstream( _( "csgo/materials/animated_wireframe.vmt" ) ) << _( R"#("UnlitGeneric" {
+	        "$basetexture" "models/inventory_items/dreamhack_trophies/dreamhack_star_blur"
+            "$wireframe" "1"
+            "$alpha" "0.6"
+            "$additive" "1"
+            "proxies"
+             {
+                "texturescroll"
+                {
+                    "texturescrollvar" "$basetexturetransform"
+                    "texturescrollrate" "0.2"
+                    "texturescrollangle" "90"
+                }
+            }
+        })#" );
+
+		animated_wireframe = g_interfaces.material_system->find_material( _( "animated_wireframe" ) );
+	}
+
 	draw_model_execute_hook.call_original< void >( ecx, edx, context, state, info, custom_bone_to_world );
+
+	g_interfaces.model_render->forced_material_override( animated_wireframe );
+
+	draw_model_execute_hook.call_original< void >( ecx, edx, context, state, info, custom_bone_to_world );
+
+	g_interfaces.model_render->forced_material_override( nullptr );
 }
