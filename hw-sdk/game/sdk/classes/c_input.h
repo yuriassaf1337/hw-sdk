2dfdbbada5c29facb4d33abd544de2a2e5b60df0
yuriassaf1337 <38926035+yuriassaf1337@users.noreply.github.com>
2022-06-23 23:13:24 -0300

    updated c input class

--- hw-sdk/game/sdk/classes/c_input.h
+++ hw-sdk/game/sdk/classes/c_input.h
@@ -17,43 +17,27 @@ namespace sdk
 	public:
 		PAD( 0xC );
 		bool trackir_available;
-		bool mouse_initiated;
-		bool mouse_active;
-		bool joystick_advanced_init;
-		PAD( 0x2C );
-		void* keys;
-		PAD( 0x6C );
-		bool camera_intercepting_mouse;
-		bool camera_in_third_person;
-		bool camera_moving_with_mouse;
-		math::vec3 camer_offset;
-		bool camera_distance_move;
-		int camera_old_x;
-		int camera_old_y;
-		int camera_x;
-		int camera_y;
-		bool camera_is_orthographic;
-		math::vec3 previous_view_angles;
-		math::vec3 preview_view_angles_tilt;
-		float last_forward_move;
-		int clear_input_state;
-		PAD( 0x18 );
-		sdk::c_user_cmd* commands;
-		sdk::c_verified_cmd* verified_commands;
+		bool mouse_initialized;          
+		bool mouse_active;               
+		PAD( 0x9A );                     
+		bool camera_in_third_person;     
+		PAD( 0x2 );                      
+		math::vec3 camera_offset;        
+		PAD( 0x38 );                     
+		sdk::c_user_cmd* commands;                 
+		sdk::c_verified_cmd* verified_commands; 
 
-		inline sdk::c_user_cmd* get_user_cmd( int slot, int sequence_number );
-		inline sdk::c_verified_cmd* get_verified_cmd( int sequence_number );
+		inline sdk::c_user_cmd* get_user_cmd( const int sequence_number );
+		inline sdk::c_verified_cmd* get_verified_cmd( const int sequence_number );
 	};
 } // namespace sdk
 
-sdk::c_user_cmd* sdk::c_input::get_user_cmd( int slot, int sequence_number )
+sdk::c_user_cmd* sdk::c_input::get_user_cmd( const int sequence_number )
 {
-	return reinterpret_cast< sdk::c_user_cmd*( __thiscall* )( void*, int, int ) >( virtual_func::get( this, 8 ) )( this, slot, sequence_number );
+	return &commands[ sequence_number % multiplayer_backup ];
 }
 
-sdk::c_verified_cmd* sdk::c_input::get_verified_cmd( int sequence_number )
+sdk::c_verified_cmd* sdk::c_input::get_verified_cmd( const int sequence_number )
 {
-	auto verified_commands = *reinterpret_cast< sdk::c_verified_cmd** >( reinterpret_cast< std::uint32_t >( this ) + 0x10C );
-
 	return &verified_commands[ sequence_number % multiplayer_backup ];
 }
--- hw-sdk/globals/hooks/create_move/create_move.cpp
+++ hw-sdk/globals/hooks/create_move/create_move.cpp
@@ -12,11 +12,11 @@
 #include "../cl_move/cl_move.h"
 #include "create_move.h"
 
-void __stdcall create_move_function( int sequence_number, float input_sample_frametime, bool active, bool& send_packet )
+void __stdcall create_move_fn( int sequence_number, float input_sample_frametime, bool active, bool& send_packet )
 {
 	hooks::create_move_hook.call_original< void >( g_interfaces.client, nullptr, sequence_number, input_sample_frametime, active );
 
-	auto command  = g_interfaces.input->get_user_cmd( 0, sequence_number );
+	auto command  = g_interfaces.input->get_user_cmd( sequence_number );
 	auto verified = g_interfaces.input->get_verified_cmd( sequence_number );
 
 	// grab global cmd
@@ -61,7 +61,7 @@ __declspec( naked ) void __fastcall hooks::create_move::create_move_detour( sdk:
 		push dword ptr[active]
 		push dword ptr[input_sample_frametime]
 		push dword ptr[sequence_number]
-		call create_move_function
+		call create_move_fn
 		pop  ebx
 		pop  ebp
 		retn 0Ch
