f39bd2599019181d293a88f62ecabf74e6481089
yuriassaf1337 <38926035+yuriassaf1337@users.noreply.github.com>
2022-07-12 21:53:38 -0300

    hooks.h

--- hw-sdk/hooks/hooks.h
+++ hw-sdk/hooks/hooks.h
@@ -4,3 +4,16 @@
 
 #include "../dependencies/minhook/include/MinHook.h"
 #include "../utils/console/console.h"
+#include "../utils/detour/detour.h"
+
+namespace hooks
+{
+	inline detour::hook create_move;
+
+	struct impl {
+		void init( );
+		void unload( );
+	};
+} // namespace hooks
+
+inline hooks::impl g_hooks;
